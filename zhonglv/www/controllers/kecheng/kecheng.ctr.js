4bfbc5e8ee06559f19eb0c1a0a29246f26565668
fanyanfang <18301317549@163.com>
2017-03-03 10:02:08 +0800

    add2

--- zhonglv/www/controllers/kecheng/kecheng.ctr.js
+++ zhonglv/www/controllers/kecheng/kecheng.ctr.js
@@ -1,3 +1,68 @@
-angular.module("app").controller("kechengController",function($scope){
+angular.module("app").controller("kechengController",function($scope,$http,$q,$timeout,$ionicLoading,httpServer,userServer){
+	
+	//头部数据
+
+	httpServer.get("Course/GetCourseCategory",{
+		user_id:1
+	},function(data){
+		$scope.kechengTitle=data.data.RetValue;
+		//console.log(data.data.RetValue)
+	},function(){
+		console.log("数据请求失败")
+	});
+	/*var deferred=$q.defer(),
+		promise=deferred.promise;
+
+	promise.then(function(){		
+		add(dataId)
+	})*/
+	list(84);
+	//列表
+	function list(id){
+		//console.log(id);
+		userServer.kechi({
+			user_id :1,
+			category_id : id,
+			pagesize :20,
+			pageindex:5
+		},function(data){
+			if(data.data.RetValue==null){
+				alert("没有数据");
+			}else{
+				//console.log(data.data.RetValue);
+				$scope.listNav=data.data.RetValue;
+			}			
+		},function(){
+			console.log("数据请求失败");
+		})
+	}
+
+
+
+
+
+
+	$scope.data = {
+        current: "84" 
+    };            
+    $scope.actions =function(param,index){
+    	 $scope.data.current = param;
+    	 list(param);
+    	 console.log(param+"===="+index)
+    }
 	
 })
+/*.run(function ($rootScope) {
+    $rootScope.data = {
+        current: "84" 
+    };            
+    $rootScope.actions =function(param,index){
+    	 $rootScope.data.current = param;
+    	 console.log(param+"===="+index)
+    }
+    {
+        setCurrent: function (param) {
+            $rootScope.data.current = param;
+        }
+    }
+})*/
